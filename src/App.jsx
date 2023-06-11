030ad8d57f7ca8ddc768014783c470de43c6b581
Prince Kumar Sharma <ipreencekmr@gmail.com>
2023-06-11 16:28:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => <div className="container">Employee</div>;
+ReactDOM.render(<App />, document.getElementById("app"));
+
+export default App;
